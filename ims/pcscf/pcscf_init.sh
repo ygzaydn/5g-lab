5d41fd69ee3f5041087c3324770a3a43862ebc3f
root <ygzaydn@gmail.com>
2026-01-28 09:10:24 +0300

    ims port & dns fixes

--- ims/pcscf/pcscf_init.sh
+++ ims/pcscf/pcscf_init.sh
@@ -1,112 +1,96 @@
 #!/bin/bash
 
-# BSD 2-Clause License
-
-# Copyright (c) 2020-2025, Supreeth Herle
-# All rights reserved.
-
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-
-# 1. Redistributions of source code must retain the above copyright notice, this
-#    list of conditions and the following disclaimer.
-
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-#    this list of conditions and the following disclaimer in the documentation
-#    and/or other materials provided with the distribution.
-
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 sh -c "echo 1 > /proc/sys/net/ipv4/ip_nonlocal_bind"
 sh -c "echo 1 > /proc/sys/net/ipv6/ip_nonlocal_bind"
 
-[ ${#MNC} == 3 ] && EPC_DOMAIN="epc.mnc${MNC}.mcc${MCC}.3gppnetwork.org" || EPC_DOMAIN="epc.mnc0${MNC}.mcc${MCC}.3gppnetwork.org"
-[ ${#MNC} == 3 ] && IMS_DOMAIN="ims.mnc${MNC}.mcc${MCC}.3gppnetwork.org" || IMS_DOMAIN="ims.mnc0${MNC}.mcc${MCC}.3gppnetwork.org"
+apt-get update && apt-get install -y iproute2 mysql-client
 
-mkdir -p /etc/kamailio_pcscf
-cp /mnt/pcscf/pcscf.cfg /etc/kamailio_pcscf
-cp /mnt/pcscf/pcscf.xml /etc/kamailio_pcscf
-cp /mnt/pcscf/kamailio_pcscf.cfg /etc/kamailio_pcscf
-cp -r /mnt/pcscf/route /etc/kamailio_pcscf
-cp -r /mnt/pcscf/sems /etc/kamailio_pcscf
-cp /mnt/pcscf/tls.cfg /etc/kamailio_pcscf
-cp /mnt/pcscf/dispatcher.list /etc/kamailio_pcscf
+[ ${#MNC} == 3 ] && IMS_DOMAIN="ims.mnc${MNC}.mcc${MCC}.3gppnetwork.org" || IMS_DOMAIN="ims.mnc0${MNC}.mcc${MCC}.3gppnetwork.org"
+[ ${#MNC} == 3 ] && EPC_DOMAIN="epc.mnc${MNC}.mcc${MCC}.3gppnetwork.org" || EPC_DOMAIN="epc.mnc0${MNC}.mcc${MCC}.3gppnetwork.org"
+
+mkdir -p /etc/kamailio_pcscf/route
+mkdir -p /etc/kamailio_pcscf/sems/etc
+
+cp /mnt/pcscf/pcscf.cfg /etc/kamailio_pcscf/
+cp /mnt/pcscf/pcscf.xml /etc/kamailio_pcscf/
+cp /mnt/pcscf/kamailio_pcscf.cfg /etc/kamailio_pcscf/
+cp /mnt/pcscf/tls.cfg /etc/kamailio_pcscf/
+cp /mnt/pcscf/dispatcher.list /etc/kamailio_pcscf/
+
+cp /mnt/pcscf/route_mo.cfg /etc/kamailio_pcscf/route/mo.cfg
+cp /mnt/pcscf/route_mt.cfg /etc/kamailio_pcscf/route/mt.cfg
+cp /mnt/pcscf/route_register.cfg /etc/kamailio_pcscf/route/register.cfg
+cp /mnt/pcscf/route_rtp.cfg /etc/kamailio_pcscf/route/rtp.cfg
+cp /mnt/pcscf/route_websocket.cfg /etc/kamailio_pcscf/route/websocket.cfg
+cp /mnt/pcscf/route_xmlrpc.cfg /etc/kamailio_pcscf/route/xmlrpc.cfg
+
+cp /mnt/pcscf/sems.conf /etc/kamailio_pcscf/sems/sems.conf
+cp /mnt/pcscf/sems_etc_methodmap.conf /etc/kamailio_pcscf/sems/etc/methodmap.conf
+cp /mnt/pcscf/sems_etc_monitoring.conf /etc/kamailio_pcscf/sems/etc/monitoring.conf
+cp /mnt/pcscf/sems_etc_mo.sbcprofile.conf /etc/kamailio_pcscf/sems/etc/mo.sbcprofile.conf
+cp /mnt/pcscf/sems_etc_mt.sbcprofile.conf /etc/kamailio_pcscf/sems/etc/mt.sbcprofile.conf
+cp /mnt/pcscf/sems_etc_nocache.sbcprofile.conf /etc/kamailio_pcscf/sems/etc/nocache.sbcprofile.conf
+cp /mnt/pcscf/sems_etc_refuse.sbcprofile.conf /etc/kamailio_pcscf/sems/etc/refuse.sbcprofile.conf
+cp /mnt/pcscf/sems_etc_refuse_with_200.sbcprofile.conf /etc/kamailio_pcscf/sems/etc/refuse_with_200.sbcprofile.conf
+cp /mnt/pcscf/sems_etc_register.sbcprofile.conf /etc/kamailio_pcscf/sems/etc/register.sbcprofile.conf
+cp /mnt/pcscf/sems_etc_rurimap.conf /etc/kamailio_pcscf/sems/etc/rurimap.conf
+cp /mnt/pcscf/sems_etc_sbc.conf /etc/kamailio_pcscf/sems/etc/sbc.conf
+cp /mnt/pcscf/sems_etc_stats.conf /etc/kamailio_pcscf/sems/etc/stats.conf
+cp /mnt/pcscf/sems_etc_xmlrpc2di.conf /etc/kamailio_pcscf/sems/etc/xmlrpc2di.conf
 
 while ! mysqladmin ping -h ${MYSQL_IP} -u root -p"${MYSQL_ROOT_PASSWORD}" --silent; do
-	sleep 5;
+    sleep 5;
 done
-
-# Sleep until permissions are set
 sleep 10;
 
-# Create PCSCF database, populate tables and grant privileges
 if [[ -z "`mysql -u root -h ${MYSQL_IP} -qfsBe "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='pcscf'" 2>&1`" ]];
 then
-	mysql -u root -h ${MYSQL_IP} -e "create database pcscf;"
-	mysql -u root -h ${MYSQL_IP} pcscf < /usr/local/src/kamailio/utils/kamctl/mysql/standard-create.sql
-	mysql -u root -h ${MYSQL_IP} pcscf < /usr/local/src/kamailio/utils/kamctl/mysql/presence-create.sql
-	mysql -u root -h ${MYSQL_IP} pcscf < /usr/local/src/kamailio/utils/kamctl/mysql/ims_usrloc_pcscf-create.sql
-	mysql -u root -h ${MYSQL_IP} pcscf < /usr/local/src/kamailio/utils/kamctl/mysql/ims_dialog-create.sql
-	PCSCF_USER_EXISTS=`mysql -u root -h ${MYSQL_IP} -s -N -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE User = 'pcscf' AND Host = '%')"`
-	if [[ "$PCSCF_USER_EXISTS" == 0 ]]
-	then
-		mysql -u root -h ${MYSQL_IP} -e "CREATE USER 'pcscf'@'%' IDENTIFIED WITH mysql_native_password BY 'heslo'";
-		mysql -u root -h ${MYSQL_IP} -e "CREATE USER 'pcscf'@'$PCSCF_IP' IDENTIFIED WITH mysql_native_password BY 'heslo'";
-		mysql -u root -h ${MYSQL_IP} -e "GRANT ALL ON pcscf.* TO 'pcscf'@'%'";
-		mysql -u root -h ${MYSQL_IP} -e "GRANT ALL ON pcscf.* TO 'pcscf'@'$PCSCF_IP'";
-		mysql -u root -h ${MYSQL_IP} -e "FLUSH PRIVILEGES;"
-	fi
+    mysql -u root -h ${MYSQL_IP} -e "create database pcscf;"
+    mysql -u root -h ${MYSQL_IP} pcscf < /usr/local/src/kamailio/utils/kamctl/mysql/standard-create.sql
+    mysql -u root -h ${MYSQL_IP} pcscf < /usr/local/src/kamailio/utils/kamctl/mysql/presence-create.sql
+    mysql -u root -h ${MYSQL_IP} pcscf < /usr/local/src/kamailio/utils/kamctl/mysql/ims_usrloc_pcscf-create.sql
+    mysql -u root -h ${MYSQL_IP} pcscf < /usr/local/src/kamailio/utils/kamctl/mysql/ims_dialog-create.sql
+    PCSCF_USER_EXISTS=`mysql -u root -h ${MYSQL_IP} -s -N -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE User = 'pcscf' AND Host = '%')"`
+    if [[ "$PCSCF_USER_EXISTS" == 0 ]]
+    then
+        mysql -u root -h ${MYSQL_IP} -e "CREATE USER 'pcscf'@'%' IDENTIFIED WITH mysql_native_password BY 'heslo'";
+        mysql -u root -h ${MYSQL_IP} -e "GRANT ALL ON pcscf.* TO 'pcscf'@'%'";
+        mysql -u root -h ${MYSQL_IP} -e "FLUSH PRIVILEGES;"
+    fi
 fi
 
 if [[ ${DEPLOY_MODE} == 5G ]];
 then
     sed -i 's|#!define WITH_RX\b|##!define WITH_RX|g' /etc/kamailio_pcscf/pcscf.cfg
-	#sed -i 's|##!define WITH_N5\b|#!define WITH_N5|g' /etc/kamailio_pcscf/pcscf.cfg
 fi
 
 SUBSCRIPTION_EXPIRES_ENV=603600
 
-sed -i 's|PCSCF_IP|'$PCSCF_IP'|g' /etc/kamailio_pcscf/pcscf.cfg
-sed -i 's|SUBSCRIPTION_EXPIRES_ENV|'$SUBSCRIPTION_EXPIRES_ENV'|g' /etc/kamailio_pcscf/pcscf.cfg
-sed -i 's|SCP_IP|'$SCP_IP'|g' /etc/kamailio_pcscf/pcscf.cfg
-sed -i 's|PCSCF_PUB_IP|'$PCSCF_PUB_IP'|g' /etc/kamailio_pcscf/pcscf.cfg
-sed -i 's|IMS_DOMAIN|'$IMS_DOMAIN'|g' /etc/kamailio_pcscf/pcscf.cfg
-sed -i 's|EPC_DOMAIN|'$EPC_DOMAIN'|g' /etc/kamailio_pcscf/pcscf.cfg
-sed -i 's|MYSQL_IP|'$MYSQL_IP'|g' /etc/kamailio_pcscf/pcscf.cfg
+sed -i "s|PCSCF_IP|$PCSCF_IP|g" /etc/kamailio_pcscf/pcscf.cfg
+sed -i "s|SUBSCRIPTION_EXPIRES_ENV|$SUBSCRIPTION_EXPIRES_ENV|g" /etc/kamailio_pcscf/pcscf.cfg
+sed -i "s|SCP_IP|$SCP_IP|g" /etc/kamailio_pcscf/pcscf.cfg
+sed -i "s|PCSCF_PUB_IP|$PCSCF_PUB_IP|g" /etc/kamailio_pcscf/pcscf.cfg
+sed -i "s|IMS_DOMAIN|$IMS_DOMAIN|g" /etc/kamailio_pcscf/pcscf.cfg
+sed -i "s|EPC_DOMAIN|$EPC_DOMAIN|g" /etc/kamailio_pcscf/pcscf.cfg
+sed -i "s|MYSQL_IP|$MYSQL_IP|g" /etc/kamailio_pcscf/pcscf.cfg
 
-sed -i 's|PCSCF_IP|'$PCSCF_IP'|g' /etc/kamailio_pcscf/pcscf.xml
-sed -i 's|SUBSCRIPTION_EXPIRES_ENV|'$SUBSCRIPTION_EXPIRES_ENV'|g' /etc/kamailio_pcscf/pcscf.xml
-sed -i 's|IMS_DOMAIN|'$IMS_DOMAIN'|g' /etc/kamailio_pcscf/pcscf.xml
-sed -i 's|EPC_DOMAIN|'$EPC_DOMAIN'|g' /etc/kamailio_pcscf/pcscf.xml
-sed -i 's|PCRF_BIND_PORT|'$PCRF_BIND_PORT'|g' /etc/kamailio_pcscf/pcscf.xml
-sed -i 's|PCSCF_BIND_PORT|'$PCSCF_BIND_PORT'|g' /etc/kamailio_pcscf/pcscf.xml
+sed -i "s|PCSCF_IP|$PCSCF_IP|g" /etc/kamailio_pcscf/pcscf.xml
+sed -i "s|SUBSCRIPTION_EXPIRES_ENV|$SUBSCRIPTION_EXPIRES_ENV|g" /etc/kamailio_pcscf/pcscf.xml
+sed -i "s|IMS_DOMAIN|$IMS_DOMAIN|g" /etc/kamailio_pcscf/pcscf.xml
+sed -i "s|EPC_DOMAIN|$EPC_DOMAIN|g" /etc/kamailio_pcscf/pcscf.xml
+sed -i "s|PCRF_BIND_PORT|$PCRF_BIND_PORT|g" /etc/kamailio_pcscf/pcscf.xml
+sed -i "s|PCSCF_BIND_PORT|$PCSCF_BIND_PORT|g" /etc/kamailio_pcscf/pcscf.xml
 
-sed -i 's|RTPENGINE_IP|'$RTPENGINE_IP'|g' /etc/kamailio_pcscf/kamailio_pcscf.cfg
-sed -i 's|RTPENGINE_IP|'$RTPENGINE_IP'|g' /etc/kamailio_pcscf/route/rtp.cfg
+sed -i "s|RTPENGINE_IP|$RTPENGINE_IP|g" /etc/kamailio_pcscf/kamailio_pcscf.cfg
+sed -i "s|RTPENGINE_IP|$RTPENGINE_IP|g" /etc/kamailio_pcscf/route/rtp.cfg
 
-# Add static route to route traffic back to UE as there is not NATing
 ip r add ${UE_IPV4_IMS} via ${UPF_IP}
-# Route needed for VoWiFi client where internet APN is used
 ip r add ${UE_IPV4_INTERNET} via ${UPF_IP}
 
-mkdir -p /var/run/kamailio_pcscf
-rm -f /kamailio_pcscf.pid
-exec kamailio -f /etc/kamailio_pcscf/kamailio_pcscf.cfg -P /kamailio_pcscf.pid -DD -E -e $@
-
-# Sync docker time
-#ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 echo "10.10.10.62 $IMS_DOMAIN" >> /etc/hosts
 echo "10.10.10.62 icscf.$IMS_DOMAIN" >> /etc/hosts
 echo "10.10.10.63 scscf.$IMS_DOMAIN" >> /etc/hosts
 
-echo "10.10.10.62 ims.mnc070.mcc999.3gppnetwork.org" >> /etc/hosts
-echo "10.10.10.62 scscf.ims.mnc070.mcc999.3gppnetwork.org" >> /etc/hosts
+mkdir -p /var/run/kamailio_pcscf
+rm -f /kamailio_pcscf.pid
+exec kamailio -f /etc/kamailio_pcscf/kamailio_pcscf.cfg -P /kamailio_pcscf.pid -DD -E -e $@
--- ims/scscf/scscf_init.sh
+++ ims/scscf/scscf_init.sh
@@ -5,7 +5,6 @@ IMS_SLASH_DOMAIN=$(echo $IMS_DOMAIN | sed 's/\./\\./g')
 
 mkdir -p /etc/kamailio_scscf
 cp /mnt/scscf/* /etc/kamailio_scscf/
-
 cd /etc/kamailio_scscf/
 
 sed -i "s|SCSCF_IP|$SCSCF_IP|g" *.cfg *.xml
@@ -18,16 +17,26 @@ sed -i "s|SCSCF_BIND_PORT|3868|g" scscf.xml
 sed -i 's|#!define DB_URL .*|#!define DB_URL "mysql://scscf:imspass@'$MYSQL_IP'/scscf"|' scscf.cfg
 
 echo "MySQL Bekleniyor..."
-while ! mysqladmin ping -h ${MYSQL_IP} -uroot -p${MYSQL_PWD} --silent; do sleep 2; done
+while ! mysqladmin ping -h ${MYSQL_IP} -uroot -p$MYSQL_PWD --silent; do sleep 2; done
 
 echo "Veritabanı ve Tablolar hazırlanıyor..."
 mysql -u root -p$MYSQL_PWD -h ${MYSQL_IP} -e "CREATE DATABASE IF NOT EXISTS scscf;"
 mysql -u root -p$MYSQL_PWD -h ${MYSQL_IP} -e "CREATE USER IF NOT EXISTS 'scscf'@'%' IDENTIFIED BY 'imspass';"
 mysql -u root -p$MYSQL_PWD -h ${MYSQL_IP} -e "GRANT ALL PRIVILEGES ON scscf.* TO 'scscf'@'%'; FLUSH PRIVILEGES;"
 
-mysql -u root -p$MYSQL_PWD -h ${MYSQL_IP} scscf < /mnt/scscf/standard-create.sql
-mysql -u root -p$MYSQL_PWD -h ${MYSQL_IP} scscf < /mnt/scscf/presence-create.sql
-mysql -u root -p$MYSQL_PWD -h ${MYSQL_IP} scscf < /mnt/scscf/ims_usrloc_scscf-create.sql
+mysql -u root -p$MYSQL_PWD -h ${MYSQL_IP} scscf < /etc/kamailio_scscf/standard-create.sql
+mysql -u root -p$MYSQL_PWD -h ${MYSQL_IP} scscf < /etc/kamailio_scscf/presence-create.sql
+mysql -u root -p$MYSQL_PWD -h ${MYSQL_IP} scscf < /etc/kamailio_scscf/ims_usrloc_scscf-create.sql
+
+if [ -f /usr/local/src/kamailio/utils/kamctl/mysql/ims_dialog-create.sql ]; then
+    mysql -u root -p$MYSQL_PWD -h ${MYSQL_IP} scscf < /usr/local/src/kamailio/utils/kamctl/mysql/ims_dialog-create.sql
+fi
+
+echo "$PYHSS_IP hss.$IMS_DOMAIN" >> /etc/hosts
+echo "$ICSCF_IP icscf.$IMS_DOMAIN" >> /etc/hosts
+echo "$SCSCF_IP scscf.$IMS_DOMAIN" >> /etc/hosts
+echo "$PCSCF_IP pcscf.$IMS_DOMAIN" >> /etc/hosts
+
 
 mkdir -p /var/run/kamailio
-exec kamailio -f /etc/kamailio_scscf/kamailio_scscf.cfg -P /var/run/kamailio/scscf.pid -DD -E
+exec kamailio -f /etc/kamailio_scscf/kamailio_scscf.cfg -P /var/run/kamailio/scscf.pid -DD -E -e $@
